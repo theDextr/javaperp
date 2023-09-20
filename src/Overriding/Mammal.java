e9a12367e9412b88a4974d12c380081731497209
theDextr <akhilsingh2709@gmail.com>
2023-09-20 06:47:07 +0530

    removed unwanted import

--- src/Overriding/Mammal.java
+++ src/Overriding/Mammal.java
@@ -1,7 +1,5 @@
 package Overriding;
 
-import java.util.Arrays;
-
 public class Mammal {
     public void eat(Mammal mammal) {
         System.out.println("Mammal is eating something, let's find out.");
